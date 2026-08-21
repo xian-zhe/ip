e7f85439e378bf18232df8048a270afe0da228f8
xian-zhe <e1511868@u.nus.edu>
2026-08-22 01:39:26 +0800

    Completed Level-3 increment

--- src/main/java/Oz.java
+++ src/main/java/Oz.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Oz {
     private static final String DIVIDER = "____________________________________________________________\n";
+    //regex patterns
+    private static final Pattern MARK_PATTERN =
+            Pattern.compile("^mark\\s+(?<index>\\d+)$");
+    private static final Pattern UNMARK_PATTERN =
+            Pattern.compile("^unmark\\s+(?<index>\\d+)$");
     public static void main(String[] args) {
         String banner = "  ___    ____ \n"
                 + " / _ \\  |_  /\n"
@@ -14,33 +21,52 @@ public class Oz {
                 "What can I do for you? ᕙ(  •̀ ᗜ •́  )ᕗ\n";
 
         String bye = DIVIDER +
-                "Bye. Hope to see you again soon! („• ֊ •„)੭\n" +
+                "  Bye. Hope to see you again soon! („• ֊ •„)੭\n" +
                 DIVIDER;
 
         System.out.println(greeting);
 
         Scanner scanner = new Scanner(System.in);
+
+
         String desc = "";
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int counter = 0;
 
         while(true) {
             desc = scanner.nextLine();
+            //set up matchers
+            Matcher markMatcher = MARK_PATTERN.matcher(desc);
+            Matcher unmarkMatcher = UNMARK_PATTERN.matcher(desc);
+            String reply = "";
             if (desc.equals("bye")) {
                 break;
             } else if(desc.equals("list")) {
-                System.out.print(DIVIDER);
+                reply += DIVIDER + "Here are the tasks in your list:\n";
                 for(int i = 0; i < counter; i++){
-                    System.out.printf("%d. %s%n",i + 1, list[i]);
+                    reply += String.format("%d. %s%n",i + 1, list[i]);
                 }
-                System.out.print(DIVIDER);
-            } else {
-                String reply = DIVIDER + "added: " + desc + "\n" + DIVIDER;
-                System.out.println(reply);
-                list[counter] = desc;
+                reply += DIVIDER;
+            } else if (markMatcher.matches()) {
+
+                int index = Integer.parseInt(markMatcher.group("index")) - 1;
+                list[index].mark();
+                reply += DIVIDER + "Nice! I've marked this task as done:\n  " + list[index] + "\n" + DIVIDER;
+
+            } else if (unmarkMatcher.matches()) {
+
+                int index = Integer.parseInt(unmarkMatcher.group("index")) - 1;
+                list[index].unmark();
+                reply += DIVIDER + "OK, I've marked this task as not done yet:\n  " + list[index] + "\n" + DIVIDER;
+
+            }else {
+                reply += DIVIDER + "added: " + desc + "\n" + DIVIDER;
+                list[counter] = new Task(desc);
                 counter++;
             }
+
+            System.out.println(reply);
 
         }
         System.out.println(bye);
