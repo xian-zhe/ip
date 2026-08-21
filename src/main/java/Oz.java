a0b1bfe2f4f61abbe3ef601ad8fe7b3dde694b08
xian-zhe <e1511868@u.nus.edu>
2026-08-22 00:26:25 +0800

    Complete Level-1 increment

--- src/main/java/Oz.java
+++ src/main/java/Oz.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Oz {
+    private static final String DIVIDER = "____________________________________________________________\n";
     public static void main(String[] args) {
         String banner = "  ___    ____ \n"
                 + " / _ \\  |_  /\n"
@@ -8,13 +11,25 @@ public class Oz {
         String greeting = "____________________________________________________________\n" +
                 banner +
                 "Hello! I'm Oz.\n" +
-                "What can I do for you?\n";
+                "What can I do for you? ᕙ(  •̀ ᗜ •́  )ᕗ\n";
 
         String bye = "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
+                "Bye. Hope to see you again soon! („• ֊ •„)੭\n" +
                 "____________________________________________________________\n";
 
         System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String desc = "";
+
+        while (true) {
+            desc = scanner.nextLine();
+            if (desc.equals("bye")){
+                break;
+            }
+            String reply = DIVIDER + desc + "\n" + DIVIDER;
+            System.out.println(reply);
+        }
         System.out.println(bye);
     }
 }
